8ebc3f907f6c8f971c63f3868b98f88ae477184b
Kishor Kumar <90588133+KishorK20@users.noreply.github.com>
2023-10-25 11:13:28 +0530

    Create Knapsack with Duplicate Items.java

---
+++ Knapsack with Duplicate Items.java
@@ -0,0 +1,61 @@
+// Given a set of N items, each with a weight and a value, represented by the array w and val respectively. Also, a knapsack with weight limit W.
+// The task is to fill the knapsack in such a way that we can get the maximum profit. Return the maximum profit.
+// Note: Each item can be taken any number of times.
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        int[] dp = new int[W + 1];
+
+        // Initialize dp array with 0s
+        for (int i = 0; i <= W; i++) {
+            dp[i] = 0;
+        }
+
+        // Fill the dp array using a bottom-up approach
+        for (int i = 0; i <= W; i++) {
+            for (int j = 0; j < N; j++) {
+                if (wt[j] <= i) {
+                    dp[i] = Math.max(dp[i], dp[i - wt[j]] + val[j]);
+                }
+            }
+        }
+
+        return dp[W];
+    }
+}
